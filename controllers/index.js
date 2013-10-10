2d8271c24ee342dffd358dff7d897d587e036ac7
Brian Falk <falk@logicparty.org>
2013-10-10 10:18:05 -0400

    read controller files automagically

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,11 @@
-exports.Root = require('./root');
+var fs = require('fs')
+  , path = require('path')
+
+fs.readdirSync(__dirname).forEach(function(file) {
+  if (file.indexOf('.js') == (file.length - 3)) {
+    var name = file.substr(0, file.indexOf('.js'))
+    name = name.charAt(0).toUpperCase() + name.slice(1)
+
+    exports[name + 'Controller'] = require(path.join(__dirname, file))
+  }
+})
